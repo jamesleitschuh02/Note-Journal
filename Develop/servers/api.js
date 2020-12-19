475bcf0dde65e97cd62c985300d044b3363b64d9
James Leitschuh <james.leitschuh02@gmail.com>
2020-12-18 19:42:14 -0600

    code for get.post and get.delete work

--- Develop/servers/api.js
+++ Develop/servers/api.js
@@ -7,4 +7,37 @@ module.exports = function(app) {
     app.get("/api/notes", (req,res) => {
         res.json(db);
     });
+
+
+    app.post("/api/notes", (req,res) => {
+        fs.readFile(path.join(__dirname, "../db/db.json"), (err,data) => {
+            let dbData = JSON.parse(data);
+            dbData.push(req.body);
+
+            for (let i = 0;i < dbData.length; i++) {
+                dbData[i].id = i;
+            };
+
+            fs.writeFile("./db/db.json", JSON.stringify(dbData), (err) => {
+                res.send(dbData);
+            });
+        });
+    });
+
+    
+    app.delete("/api/notes/:id", (req,res) => {
+        fs.readFile("./db/db.json", (err,data) =>{           
+            let dbData = JSON.parse(data);
+
+            for (let i=0; i<dbData.length; i++) {
+                if (i == req.params.id){
+                    dbData.splice(i,1);
+                }
+            };
+
+            fs.writeFile("./db/db.json", JSON.stringify(dbData), (err) => {
+                res.send(dbData); 
+            });
+        });          
+    });   
 };
